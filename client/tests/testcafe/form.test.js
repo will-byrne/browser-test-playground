db084d4584e95632885019dc3d7b706fb6f644b2
Will Byrne <w.byrne@elifesciences.org>
2019-10-23 17:13:44 +0100

    fix testcafe and remove ts version

--- client/tests/testcafe/form.test.js
+++ client/tests/testcafe/form.test.js
@@ -8,11 +8,11 @@ const colourOptions = colourSelect.find("option");
 test("Can submit", async t => {
   await t
     .typeText(Selector("input[name=name]"), "Sparkles the Unicorn")
-    .typeText(Selector("input[name=age]"), 9000)
+    .typeText(Selector("input[name=age]"), "9000")
     .click(colourSelect)
-    .click(colourOptions.withText("glitter"))
-    .expect(Selector("h2"), { timeout: 2000 })
-    .expect(Selector("h2").textContent)
+    .click(colourOptions.withText("Glitter"))
+    .click("button")
+    .expect(Selector("h2").textContent, { timeout: 2000 })
     .eql(
       "Recieved Sparkles the Unicorn who is 9000 years old and their favourite colour is glitter"
     );
